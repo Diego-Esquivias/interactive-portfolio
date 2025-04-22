c5201b92c9936cd4adbd10cb51601e05808f1348
Diego-Esquivias <desqui048@west-mec.org>
2025-04-22 15:19:55 -0700

    tried to do some text animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,15 +6,18 @@ export default function Home() {
     <>
     
     {/* Navbar */}
-    <div className="text-center">
+    <div className="flex justify-between items-center text-3xl px-4 font-extrabold">
+        <h1>• Home</h1>
         <h1 className="text-[#FFC3CB]">Diego Esquivias</h1>
-        <HiOutlineMenu />
+        <HiOutlineMenu className="cursor-pointer hover:text-[#FFC3CB] transition duration-250 ease-in-out" size={50} />
     </div>
 
 
 
-    <div className="">
-      Home Testing
+    <div className="text-center animate-marquee">
+      <span>
+        Welcome To My Portfolio • Welcome To My Portfolio • Welcome To My Portfolio •
+      </span>
     </div>
       
     </>
