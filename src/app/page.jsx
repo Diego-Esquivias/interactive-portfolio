4bfcb7a32f6581900ac9079ee98b21594c078483
Diego-Esquivias <desqui048@west-mec.org>
2025-04-22 14:36:26 -0700

    setup

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { HiOutlineMenu } from "react-icons/hi";
+
+export default function Home() {
+  return (
+    <>
+    
+    {/* Navbar */}
+    <div className="text-center">
+        <h1 className="text-[#FFC3CB]">Diego Esquivias</h1>
+        <HiOutlineMenu />
+    </div>
+
+
+
+    <div className="">
+      Home Testing
+    </div>
+      
+    </>
+  );
+}
