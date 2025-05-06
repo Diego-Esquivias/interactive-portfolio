3dc7391955f2d5a04c50c0ea82e7ad326f3ad937
Diego-Esquivias <desqui048@west-mec.org>
2025-05-06 15:16:07 -0700

    Contact Page
    - Got the submit function to successfully send an email to me
    - started styling the form
    - need to work on it resting the form to show it successfully submitted

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+"use client"
 
-const contact = () => {
-    return (
-        <>
-            <div>Contact Page</div>        
-        </>
-    )
-}
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-export default contact
+export default function ContactUs() {
+const form = useRef();
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm('service_tftra6o', 'template_dlv433r', form.current, {
+        publicKey: 'YsymmhDppur3l7-9f',
+    })
+    .then(
+        () => {
+        console.log('SUCCESS!');
+        },
+        (error) => {
+        console.log('FAILED...', error.text);
+        },
+    );
+};
+
+return (
+    <form ref={form} onSubmit={sendEmail} className='bg-gray-500 py-10 rounded-2xl text-black w-1/2 mx-auto grid grid-cols-1 grid-rows-5 place-items-center'>
+        <div>
+            <label>Title</label>
+            <input type="text" name="title" className='bg-white rounded-xl'/>
+        </div>
+        <br />
+
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" className='bg-white rounded-xl'/>
+        </div>
+        <br />
+
+        <div>
+            <label>Email</label>
+            <input type="email" name="email" className='bg-white rounded-xl'/>
+        </div>
+        <br />
+
+        <div>
+            <label>Message</label>
+            <textarea name="message" className='bg-white rounded-xl'/>
+        </div>
+        <br />
+
+        <div>
+            <button type="submit">Send</button>
+        </div>
+    {/* <input type="submit" value="Send" /> */}
+    </form>
+);
+};
